f76fdc8cb65efb0ab985ebe168080de19c5dc56e
thestbar <stavrosbarousis@gmail.com>
2023-06-25 22:21:37 +0300

    Fixed the way nav bar updates when user scrolls

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
+import { useRef } from 'react'
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
 import About from './components/about/About'
-// import Experience from './components/experience/Experience'
 import Projects from './components/projects/Projects'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
 
 const App = () => {
+  const homeRef = useRef();
+  const aboutRef = useRef();
+  const projectsRef = useRef();
+  const contactRef = useRef();
   return (
     <>
-        <Header />
-        <Nav />
-        <About />
-        {/* <Experience /> */}
-        <Projects />
-        <Contact />
+        <Header ref={homeRef} />
+        <Nav homeRef={homeRef} aboutRef={aboutRef} projectsRef={projectsRef} contactRef={contactRef}/>
+        <About ref={aboutRef}/>
+        <Projects ref={projectsRef}/>
+        <Contact ref={contactRef}/>
         <Footer />
     </>
   )
